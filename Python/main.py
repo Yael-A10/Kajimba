ebdfcfdc370251c3ce5945a1b3a070ae8ab1209c
Yael-A10 <yabuv2@gmail.com>
2022-12-06 18:54:58 +0530

    main.py Cleanup
    -pumpGroup made into dictionary
    -separated main loop into functions
    -better variable naming

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,9 @@
 import time
 import machine
-
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
+#setup
 I2C_ADDR     = 0x27
 I2C_NUM_ROWS = 4
 I2C_NUM_COLS = 20
@@ -19,7 +19,8 @@ log.close()
 flow_meter = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_UP)
 relay = machine.Pin(16, machine.Pin.OUT)
 
-groups = [[relay, flow_meter, 0, False, True]]
+#groups = [[relay, flow_meter, 0, False, True]]
+pumpGroups = {"relay": [relay], "flowSensor": [flow_meter], "count": [0], "state":[False], "done": [True]}
 
 """count = 0
 def countPulse(pin):
@@ -36,14 +37,59 @@ def counter(fm, counter, state):
         state = True
     return counter, state
 
-def display():
+def writeToScreen(str1, str2):
     lcd.clear()
     lcd.move_to(0,0)
-    lcd.putstr("Filled: " + str(session_fill_count))
+    lcd.putstr(str1)
     lcd.move_to(0,1)
-    lcd.putstr("Total: " + str(total_fill_count))
+    lcd.putstr(str2)
 
-ml = 100
+def runPumps():
+    for group in range(0,len(pumpGroups["relay"])):
+        pumpGroups["relay"][group].value(1)
+    writeToScreen("Clearing...", "")
+    time.sleep(1)
+    while button.value() == 0:
+        pass
+    for group in range(0,len(pumpGroups["relay"])):
+        pumpGroups["relay"][group].value(0)
+    display()
+
+def resetPumpValues():
+    for group in range(0,len(pumpGroups["relay"])):
+        pumpGroups["relay"][group].value(1)
+        pumpGroups["count"][group] = 0
+        pumpGroups["state"][group] = False
+        pumpGroups["done"][group] = True
+
+def checkIfDone():
+    global session_fill_count
+    global total_fill_count
+    done = 0
+    while done<len(pumpGroups["relay"]):
+        for group in range(0,len(pumpGroups["relay"])):
+            if pumpGroups["done"][group]:
+                result = counter(pumpGroups["flowSensor"][group], pumpGroups["count"][group], pumpGroups["state"][group])
+                pumpGroups["count"][group] = result[0]
+                if result[0]/4 >= ml:
+                    pumpGroups["relay"][group].value(0)
+                    pumpGroups["done"][group] = False
+                    done += 1
+                    lcd.move_to(8,1)
+                    lcd.putstr(str(done))
+                    session_fill_count+=1
+                    total_fill_count+=1
+                pumpGroups["state"][group] = result[1]
+
+def writeToLog():
+    log = open("logs.txt", "w")
+    log.write(str(total_fill_count))
+    log.close()
+
+def display():
+    writeToScreen("Filled: " + str(session_fill_count),"Total: " + str(total_fill_count))
+    
+ml = 300
 session_fill_count = 0
 display()
 
@@ -51,57 +97,20 @@ try:
     while True:
         if button.value() == 1:
             start = time.time()
-            lcd.move_to(0,0)
-            lcd.putstr("Hold 3 s to")
-            lcd.move_to(0,1)
-            lcd.putstr("start clearing")
+            writeToScreen("Hold 3 s to", "Start clearing")
             while button.value() == 1 and time.time()-start < 3:
                 pass
             if time.time()-start >= 3:
-                for x in groups:
-                    x[0].value(1)
-                lcd.clear()
-                lcd.move_to(0,0)
-                lcd.putstr("Clearing...")
-                time.sleep(1)
-                while button.value() == 0:
-                    pass
-                for x in groups:
-                    x[0].value(0)
-                display()
+                runPumps()
             else:
-                lcd.clear()
-                lcd.move_to(0,0)
-                lcd.putstr("Filling...")
-                lcd.move_to(0,1)
-                lcd.putstr("Filled: 0/" + str(len(groups)))
-                for x in groups:
-                    x[0].value(1)
-                    x[2] = 0
-                    x[3] = False
-                    x[4] = True
-                done = 0
-                while done<len(groups):
-                    for x in groups:
-                        if x[4]:
-                            result = counter(x[1], x[2], x[3])
-                            x[2] = result[0]
-                            if result[0]/4 >= ml:
-                                x[0].value(0)
-                                x[4] = False
-                                done += 1
-                                lcd.move_to(8,1)
-                                lcd.putstr(str(done))
-                                session_fill_count+=1
-                                total_fill_count+=1
-                            x[3] = result[1]
-                log = open("logs.txt", "w")
-                log.write(str(total_fill_count))
-                log.close()
+                writeToScreen("Filling...", "Filled: 0/" + str(len(pumpGroups["relay"])))
+                resetPumpValues()
+                checkIfDone()
+                writeToLog()
                 time.sleep(1)
                 display()
 except KeyboardInterrupt:
-    for x in groups:
-        x[0].value(0)
+    for group in range(0,len(pumpGroups["relay"])):
+        pumpGroups["relay"][group].value(0)
     lcd.clear()
     log.close()
